21ace1478a984d87d5ef416c5a0eb414af2bc564
shifa gafoor <shifagafoorncas@gmail.com>
2025-02-28 21:37:22 +0530

    .

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const inputBox = document.querySelector("#input-box") ;
-const listContainer = document.querySelector("#list-container");
 console.log(inputBox)
+const listContainer = document.querySelector("#list-container");
+console.log(listContainer)
 function addTask(){
     if(inputBox.value === ''){
         alert("You must write something");
     }else{
         let li = document.createElement('li');
         li.innerHTML = inputBox.value;
+        inputBox.value ='';
         li.addEventListener("click", () => {
             li.classList.toggle("checked")
         } )
